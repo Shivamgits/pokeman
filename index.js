d119dbbaa498aaae2b7613774594bf3370119f1e
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2023-01-21 02:17:50 +0530

    02:17

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const next = document .getElementById("next");
 let url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
 let res = null
 let count = 0;
+const bcol = {
+  bug : "#26de81",
+  dragon : "#ffeaa7",
+  electric : "#fed330",
+  fairy : "#FF0069",
+  fighting : "#30336b",
+  fire : "#f0932b", 
+  flying : "#81ecec", 
+  grass : "#00b894", 
+  ground : "#EFB549", 
+  ghost : "#a55eea", 
+  ice : "#74b9ff", 
+  normal : "#95afc0", 
+  poison : "#6c5ce7", 
+  psychic : "# a29bfe", 
+  rock : "#2d3436", 
+  water : "#0190FF"
+}
 async function fetchapi(){
      let data = await fetch(url);
      res = await data.json();
@@ -12,9 +30,11 @@ async function fetchapi(){
     const data1 = await fetch(url1);
     count++;
     const res1 = await data1.json();
-     //console.log(res1);
+    const color=bcol[res1.types[0].type.name];
+    //  console.log(res1);
       const div = document.createElement("div")
       div.setAttribute("class", "container1");
+      div.style.background = `radial-gradient(circle at 50% 0%, ${color} 40%, #ffffff 36%)`
 
       const innerdiv = document.createElement("div");
       innerdiv.setAttribute("class", "card");
@@ -23,7 +43,8 @@ async function fetchapi(){
       const hp = document.createElement("div")
       innerdiv.append(hp);
        hp.innerText= ` hp : ${res1.stats[0].base_stat}`
-         
+       hp.setAttribute("class", "hp");
+                
        const imageDiv = document.createElement("div")
       innerdiv.append(imageDiv);
 
@@ -36,6 +57,20 @@ async function fetchapi(){
       innerdiv.append(name);
       name.setAttribute("class","name")
       name.innerText=res.results[i].name;
+
+      const type = document.createElement("div")
+      innerdiv.append(type);
+      type.setAttribute("class","type")
+      //console.log(res1.types.length)
+      for(let i=0;i<res1.types.length;i++){
+        const types = document.createElement("div")
+        type.append(types);
+        types.setAttribute("class","types")
+        types.innerText = res1.types[i].type.name
+        
+        // console.log(color);
+        types.style.background=color;
+      }
 
        const stats = document.createElement("div")
        innerdiv.append(stats);
@@ -54,10 +89,7 @@ async function fetchapi(){
          speed.innerText= ` speed  ${res1.stats[5].base_stat}`
       
        cont.append(div)
-    }
-  
-     // console.log(url)
-    
+    }  
 }
 
 async function nextPage(){
